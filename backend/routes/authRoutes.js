5fb546b1a4be043e1bf88e3819ceafa70ec80bce
josh <joshlagrimas1808@gmail.com>
2025-05-09 22:18:29 +0800

    feat(auth): add signup (no otp yet)

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,7 +7,7 @@ import {
 } from "../controllers/authControllers.js";
 
 const router = express.Router();
-
+ 
 router.post("/signup", signupRoute);
 router.post("/verify-email", verifyEmail);
 router.post("/login", loginRoute);
--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,12 @@ app.use(morgan("dev"));
 app.use(cors());
 
 // routes
-
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
 
 // start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-})
+});
