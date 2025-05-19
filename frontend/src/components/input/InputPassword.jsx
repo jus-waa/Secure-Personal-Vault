ad6203e296eaeddde71dc3124cebd3238b1bf0bf
josh <joshlagrimas1808@gmail.com>
2025-05-20 01:04:34 +0800

    feat(login): implement login page ui

--- frontend/src/components/input/InputPassword.jsx
+++ frontend/src/components/input/InputPassword.jsx
@@ -1,22 +1,34 @@
 import React, { useState } from 'react';
-
+import { FaRegEye,FaRegEyeSlash } from "react-icons/fa6";
 const InputPassword = ({ value, onChange, placeholder}) => {
     const [ShowPassword, setShowPassword] = useState(false);
-
     const toggleShowPassword = () => {
         setShowPassword(!ShowPassword);
     };
   
     return (
     <>
-        <div className='input-box'>
+        <div className='flex input-box'>
             <input 
-            value={value}
-            onChange={onChange}
-            type={ShowPassword ? "text" : "password"}
-            placeholder={placeholder||"Password"}
-            className='w-full text-sm bg-transparent rounded outline-none'
+             value={value}
+             onChange={onChange}
+             type={ShowPassword ? "text" : "password"}
+             placeholder={placeholder||"Password"}
+             className='w-full text-sm bg-transparent rounded outline-none'
             />
+
+            {ShowPassword ? (<FaRegEye
+             size={20}
+             className="text-primary cursor-pointer"
+             onClick={toggleShowPassword}
+            />
+            ) : (
+            <FaRegEyeSlash
+             size={20}
+             className="text-primary cursor-pointer"
+             onClick={toggleShowPassword}
+            />
+            )}
         </div>
     </>
   );
--- frontend/src/routes/LoginPage.jsx
+++ frontend/src/routes/LoginPage.jsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import InputPassword from "../components/input/InputPassword";
+
+import { LuBookLock } from "react-icons/lu";
+
 const LoginPage = () => {
-    function handleSubmit(e) {
+    
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+    
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
     }
-      
+    
     return(
         <>
             <div className="h-screen w-screen flex flex-col place-content-center items-center">
-            <h1 className="mb-4 text-4xl">LockPad</h1>
-                <div className="grid h-2/5 w-3/12 justify-center border-2 rounded">
-                    <div className="border-2 w-full">
+            <h1 className="flex mb-4 text-4xl"><LuBookLock />LockPad</h1>
+                <div className="grid grid-rows-[1fr_4fr] h-96 w-3/12 border-2 rounded">
+                    <h1 className="place-content-center text-2xl pl-8 pt-8 pb-2">
+                        Sign In
+                        <p className="text-xs text-gray-400">Welcome! Please enter your details.</p>
+                    </h1>
+                    <div className="w-full px-8 py-4">
                         <form onSubmit={handleSubmit}>
-                            <h1 className="text-2xl">Login</h1>
-                            {/* input for email*/}
-                            <input type="text" placeholder="Email" className="input-box" />
-                            {/* input for password*/}
-                            <InputPassword/>
+                            <input
+                             type="text"
+                             placeholder="Email" 
+                             className="input-box" 
+                             value={email}
+                             onChange={(em) => setEmail(em.target.value)} 
+                            />
+
+                            <InputPassword
+                             value={password}
+                             onChange={(em) => setPassword(em.target.value)}
+                            />
+
                             <button type="submit" className="btn-primary">
                                 Login
                             </button>
+
                             <div className="text-sm text-center mt-4">
                                 Don't have an Account?{" "}
                                 <Link to="/signUp" className="font-semibold text-primary underline">
