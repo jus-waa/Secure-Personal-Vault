c9a0d4bb5c53d7d8c8ce4e77d9f4dbd94f2771d9
fjconn <franzjcon.inserto@cvsu.edu.ph>
2025-05-27 22:54:46 +0800

    feat(nav): added side nav

--- frontend/src/components/CloudDesign.jsx
+++ frontend/src/components/CloudDesign.jsx
@@ -5,32 +5,31 @@ const CloudDesign = () => {
     <>
         <div className='fixed h-screen w-screen '>
             <div>
-  {/* Bottom Left Group */}
-  <div className='bg-gradient-to-bl from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-64 w-64 -left-40 bottom-96'></div>
-  <div className='bg-gradient-to-bl from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-80 w-80 -left-36 bottom-48'></div>
-  <div className='bg-gradient-to-br from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-80 w-80 -left-24 -bottom-20'></div>
-  <div className='bg-gradient-to-bl from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-96 w-96 -left-8 bottom-0'></div>
-  <div className='bg-gradient-to-bl from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-96 w-96 left-32 -bottom-48'></div>
-  <div className='bg-gradient-to-bl from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-64 w-64 left-96 -bottom-48'></div>
+                {/* Bottom Left Group */}
+                <div className='bg-gradient-to-bl from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-64 w-64 -left-40  bottom-96'></div>
+                <div className='bg-gradient-to-bl from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-80 w-80 -left-36 bottom-48'></div>
+                <div className='bg-gradient-to-br from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-80 w-80 -left-24 -bottom-20'></div>
+                <div className='bg-gradient-to-bl from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-96 w-96 -left-8 bottom-0'></div>
+                <div className='bg-gradient-to-bl from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-96 w-96 left-32 -bottom-48'></div>
+                <div className='bg-gradient-to-bl from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-64 w-64 left-96 -bottom-48'></div>
 
-  {/* Middle Right Group */}
-  <div className='z-0'>
-    <div className='bg-gradient-to-br from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-64 w-64 right-[30rem] bottom-64'></div>
-    <div className='bg-gradient-to-br from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-64 w-64 right-[42rem] bottom-20'></div>
-    <div className='bg-gradient-to-br from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-80 w-80 right-[30rem] bottom-24'></div>
-  </div>
+                {/* Middle Right Group */}
+                <div className='z-0'>
+                  <div className='bg-gradient-to-br from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-64 w-64 right-[30rem] bottom-64'></div>
+                  <div className='bg-gradient-to-br from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-64 w-64 right-[42rem] bottom-20'></div>
+                  <div className='bg-gradient-to-br from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-80 w-80 right-[30rem] bottom-24'></div>
+                </div>
 
-  {/* Top Right Group */}
-  <div>
-    <div className='bg-gradient-to-tr from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-64 w-64 -right-40 top-96'></div>
-    <div className='bg-gradient-to-tr from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-80 w-80 -right-36 top-48'></div>
-    <div className='bg-gradient-to-tl from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-80 w-80 -right-24 -top-20'></div>
-    <div className='bg-gradient-to-tr from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-96 w-96 -right-8 top-0'></div>
-    <div className='bg-gradient-to-tr from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-96 w-96 right-32 -top-48'></div>
-    <div className='bg-gradient-to-tr from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-64 w-64 right-96 -top-48'></div>
-  </div>
-</div>
-
+                {/* Top Right Group */}
+                <div>
+                  <div className='bg-gradient-to-tr from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-64 w-64 -right-40 top-96'></div>
+                  <div className='bg-gradient-to-tr from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-80 w-80 -right-36 top-48'></div>
+                  <div className='bg-gradient-to-tl from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-80 w-80 -right-24 -top-20'></div>
+                  <div className='bg-gradient-to-tr from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-96 w-96 -right-8 top-0'></div>
+                  <div className='bg-gradient-to-tr from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-96 w-96 right-32 -top-48'></div>
+                  <div className='bg-gradient-to-tr from-yellow-100 via-orange-200 to-orange-300 absolute rounded-[50%] h-64 w-64 right-96 -top-48'></div>
+                </div>
+            </div>
         </div>
     </>
   )
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,13 @@ import './index.css';
 import LoginPage from "./routes/LoginPage";
 import SignUpPage from "./routes/SignUpPage";
 import HomePage from "./routes/Homepage";
+import SideNav from './components/SideNav';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <Router>
             <Routes>
+                <Route path="/" element={<SideNav/>}/>
                 <Route path="/login" element={<LoginPage/>}/>
                 <Route path="/signUp" element={<SignUpPage/>}/>
                 <Route path="/home" element={<HomePage/>}/>
