28ad1ca292934509ebfa0dc8c6a447951b41ddee
josh <joshlagrimas1808@gmail.com>
2025-06-01 15:21:08 +0800

    feat(backend): setup database

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,14 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
 
-//import authRoutes from "./routes/authRoutes.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
@@ -17,11 +18,9 @@ app.use(helmet());
 app.use(morgan("dev"));
 
 //Routes
-//app.use("/api/v1/auth", authRoutes);
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server running on port ${PORT}`);
 });
