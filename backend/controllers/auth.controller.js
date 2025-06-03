3dd424383b4adb8d15dcaad338660bd8edf8a25f
Burinto <brentolym@gmail.com>
2025-06-03 13:23:51 +0800

    test

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcryptjs from "bcryptjs";
+///test commit again 2
 import { User } from "../models/user.model.js";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
 import { 
